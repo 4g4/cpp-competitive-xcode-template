0b3adc174a28ba3842de7611bc8a6844cb3e670b
4g4 <lispainter@163.com>
2025-12-11 10:27:21 +0800

    add cpp compile flags

--- Competitive Programming.xctemplate/main_input_version.cpp
+++ Competitive Programming.xctemplate/main_input_version.cpp
@@ -1,71 +1,130 @@
-//
-//  ___FILENAME___
 //  ___PACKAGENAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//
+//  Created on ___DATE___.
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <set>
-#include <map>
-#include <queue>
-#include <deque>
-#include <algorithm>
-#include <cmath>
-#include <iomanip>
-#include <string.h>
-
-// Shortcuts for "common" data types in contests
-typedef long long ll;
-typedef unsigned long long ull;
-typedef std::vector<int> vi;
-typedef std::vector<ll> vll;
-typedef std::vector<ull> vull;
-typedef std::pair<int, int> pii;
-typedef std::vector<pii> vpii;
-typedef std::set<int> si;
-typedef std::map<std::string, int> msi;
-
-// To simplify repetitions/loops, Note: define your loop style and stick with it!
-#define REP(i, a, b) \
-for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local!
-#define TRvi(c, it) \
-for (vi::iterator it = (c).begin(); it != (c).end(); it++)
-#define TRvii(c, it) \
-for (vii::iterator it = (c).begin(); it != (c).end(); it++)
-#define TRmsi(c, it) \
-for (msi::iterator it = (c).begin(); it != (c).end(); it++)
-#define INF 2000000000 // 2 billion
-
-// If you need to recall how to use memset:
-#define MEMSET_INF 127 // about 2B
-#define MEMSET_HALF_INF 63 // about 1B
-//memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
-//memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
-//memset(arr, 0, sizeof arr); // useful to clear array of integers
-
-
-#pragma mark - MAIN
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    
-    #ifdef USE_INPUT_FILE
-    freopen("input.txt", "r", stdin);
-    #endif
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VL;
+typedef vector<VL> VVL;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
 
-    std::ios::sync_with_stdio(false);
-    cout << fixed << setprecision(12);
-    cin.tie(NULL);
-    
-    // MAIN Begin
-    
-    <#Write your code here...#>
-    
-    // MAIN End
-    
-    return 0;
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998244353;
+const ll INFL = 0x3f3f3f3f3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+
+const ull Pr = 131, Pr1 = 13331;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+#define No() cout << "No\n"
+#define Yes() cout << "Yes\n"
+#define NO() cout << "NO\n"
+#define YES() cout << "YES\n"
+
+template <class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+template <class T> ostream &operator<<=(ostream &os, const vector<T> &a) {
+  int n = int(a.size()) - 1;
+  for (int i = 1; i <= n; ++i) {
+    os << a[i] << " \n"[i == n];
+  }
+  return os;
+}
+
+template <class T> ostream &operator<<(ostream &os, const vector<T> &a) {
+  int n = int(a.size());
+  for (int i = 0; i < n; ++i) {
+    os << a[i] << " \n"[i == n - 1];
+  }
+  return os;
+}
+
+template <class T> istream &operator>>(istream &is, vector<T> &a) {
+  for (auto &x : a)
+    is >> x;
+  return is;
+}
+
+template <class T> istream &operator>>=(istream &is, vector<T> &a) {
+  int n = int(a.size()) - 1;
+  for (int i = 1; i <= n; ++i) {
+    is >> a[i];
+  }
+  return is;
+}
+
+#ifdef MYDEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#endif
+
+void Init();
+void solve();
+
+int main(void) {
+#ifdef USE_INPUT_FILE
+  freopen("input.txt", "r", stdin);
+#endif
+
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+#if defined(MYDEBUG)
+  auto _start_ts = std::chrono::high_resolution_clock::now();
+#endif
+
+  int T = 1;
+  // cin >> T;
+  while (T--) {
+    Init();
+    solve();
+  }
+
+#if defined(MYDEBUG)
+  auto _end_ts = std::chrono::high_resolution_clock::now();
+  std::cerr << "Execution time: "
+            << std::chrono::duration<double>(_end_ts - _start_ts).count()
+            << " seconds." << std::endl;
+#endif
+
+  return 0;
+}
+
+#pragma mark - SOLVE
+
+void Init() {}
+
+void solve() {
+  <#Write your code here...#>
 }
